0ce4b038c99f151d0c762109c7a2a0a9163dd9bf
pauldavid23 <paul.david.gelario@gmail.com>
2026-04-15 23:50:35 +0800

    Add project files.

---
+++ RetirementCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter age now");
+int myAge = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter retirement age in your country");
+int retirementAge = int.Parse(Console.ReadLine());
+
+int yearsToRetirement = retirementAge - myAge;
+
+Console.WriteLine($"You have {yearsToRetirement} years until you can retire.");
+
+Console.ReadKey(true);
