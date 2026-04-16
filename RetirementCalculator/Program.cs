6d824466d75cd7fbc7842e13a2721c75e3cf3db4
pauldavid23 <paul.david.gelario@gmail.com>
2026-04-16 20:20:20 +0800

    Adding these changes.

--- RetirementCalculator/Program.cs
+++ RetirementCalculator/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Hi, what is your name? ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}!");
+
 Console.WriteLine("Welcome to the Retirement Calculator! \n Enter your age: ");
 int myAge = int.Parse(Console.ReadLine());
 
@@ -11,3 +15,5 @@ int yearsToRetirement = retirementAge - myAge;
 Console.WriteLine($"You have {yearsToRetirement} years until you can retire.");
 
 Console.ReadKey(true);
+
+Console.WriteLine("Press any key to exit...");
